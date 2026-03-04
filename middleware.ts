2e7047b9a692200807f57edc4866b9da89e4d09d
t445273 <t_piyawat.sritavong@scb.co.th>
2026-03-04 11:36:12 +0700

    search splunk

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,23 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const ip = request.headers.get('x-forwarded-for') || request.headers.get('x-real-ip') || 'unknown';
+  const ip = request.headers.get('x-forwarded-for')?.split(',')[0] || request.headers.get('x-real-ip') || 'unknown';
+  
   const logData = {
     event: {
       url: request.url,
       method: request.method,
       ip: ip,
       userAgent: request.headers.get('user-agent'),
-      timestamp: new Date().toISOString()
+      timestamp: new Date().toISOString(),
+      source: "vercel-middleware"
     }
   };
 
   fetch('https://intelligential-arseniuretted-braelyn.ngrok-free.dev/services/collector/event', {
     method: 'POST',
     headers: {
-      'Authorization': '98fefb7a-8af5-4274-8938-606dd0ed6861',
+      'Authorization': 'Splunk 98fefb7a-8af5-4274-8938-606dd0ed6861', 
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(logData)
