d7fe7f3a6f36d037dba1b90c341cb057d926eed4
t445273 <t_piyawat.sritavong@scb.co.th>
2026-03-04 10:55:19 +0700

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const logData = {
+    event: {
+      url: request.url,
+      method: request.method,
+      ip: request.ip || 'unknown',
+      userAgent: request.headers.get('user-agent'),
+      timestamp: new Date().toISOString()
+    }
+  };
+
+  fetch('https://intelligential-arseniuretted-braelyn.ngrok-free.dev/services/collector/event', {
+    method: 'POST',
+    headers: {
+      'Authorization': '98fefb7a-8af5-4274-8938-606dd0ed6861',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(logData)
+  }).catch((err) => console.error('Splunk Log Error:', err));
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+}
